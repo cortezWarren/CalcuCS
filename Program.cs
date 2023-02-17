084e6bb27f25a7aaae3e524749cfe24d806fa3f9
cortezWarren <92365206+cortezWarren@users.noreply.github.com>
2023-02-17 13:42:29 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using calcCs.calculation;
+namespace calcCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            calculator.runCalc();
+        }
+    }
+}
---
+++ calculation/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcCs.calculation
+{
+    class Calculator
+    {
+        public void runCalc()
+        {
+            int num1;
+            int num2;
+            string retry;
+
+            Console.WriteLine("Enter first number");
+            num1 = getCalc();
+        }
+
+        private int getCalc()
+        {
+            int val;
+            bool parseCheck = Int32.TryParse(Console.ReadLine(), out val);
+
+            while (!parseCheck)
+            {
+                Console.WriteLine("Enter numbers only");
+                parseCheck = Int32.TryParse(Console.ReadLine(), out val);
+
+            }
+            return val;
+        }
+    }
+}
